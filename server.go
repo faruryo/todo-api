f8e9a36aac0e1be29aea7b5a0642df363a8fb7cf
faruryo <faruryo@gmail.com>
2020-09-27 22:08:30 +0900

    EP変更

--- server.go
+++ server.go
@@ -6,12 +6,13 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/faruryo/toban-api/databases"
 	"github.com/faruryo/toban-api/graph/generated"
 	"github.com/faruryo/toban-api/repositories"
 	"github.com/faruryo/toban-api/resolvers"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 const defaultPort = "8080"
@@ -22,26 +23,58 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
+	debugEcho := false
+	if os.Getenv("DEBUG_ECHO") != "" {
+		debugEcho = true
+	}
+	debugDb := false
+	if os.Getenv("DEBUG_DB") != "" {
+		debugDb = true
+	}
 
-	db, err := databases.GetDbByDsn(dsn, databases.Info)
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	if debugEcho {
+		e.Use(middleware.Logger())
+	}
+	e.Use(middleware.Gzip())
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.NoContent(http.StatusOK)
+	})
+
+	logLevel := databases.Silent
+	if debugDb {
+		logLevel = databases.Info
+	}
+	db, err := databases.GetDbByDsn(dsn, logLevel)
 	if err != nil {
 		log.Printf("failed to connect database: %v", err)
 		return
 	}
 
-	srv := handler.NewDefaultServer(
-		generated.NewExecutableSchema(generated.Config{
+	gqlEp := "graphql"
+	plgEp := "playground"
+	e.POST("/"+gqlEp, func(c echo.Context) error {
+		h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{
 			Resolvers:  &resolvers.Resolver{TobanRepository: repositories.NewTobanRepository(db)},
 			Directives: generated.DirectiveRoot{},
 			Complexity: generated.ComplexityRoot{},
-		}),
-	)
+		}))
+		h.ServeHTTP(c.Response(), c.Request())
 
-	srv.Use(extension.FixedComplexityLimit(300))
+		return nil
+	})
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	e.GET("/"+plgEp, func(c echo.Context) error {
+		h := playground.Handler("GraphQL playground", "/"+gqlEp)
+		h.ServeHTTP(c.Response(), c.Request())
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Print(http.ListenAndServe(":"+port, nil))
+		return nil
+	})
+
+	e.HideBanner = true
+	e.Logger.Infof("connect to http://localhost:%s/%s for GraphQL playground", port, plgEp)
+	e.Logger.Fatal(e.Start(":" + port))
 }
